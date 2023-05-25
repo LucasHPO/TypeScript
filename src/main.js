1ad6b6505b957f7b893d16c022c3ebf60c809640
Lucas Hiago <lucashiago620@hotmail.com>
2023-05-25 15:48:51 -0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,13 @@
+"use strict";
+// Função de multiplicação
+// function multiplicar(a: number, b: number):number{
+//     return a * b;
+// }
+const multiplicar = (a, b) => { return a * b; };
+console.log(multiplicar(9, 5));
+// Função de saudação
+// function ola (nome: String): string {
+//     return "Ola " + nome;
+// }
+const ola = (nome) => { return "Olá " + nome; };
+console.log(ola('Ronaldo'));
---
+++ src/main.ts
@@ -0,0 +1,20 @@
+
+// Função de multiplicação //
+
+// function multiplicar(a: number, b: number):number{
+//     return a * b;
+// }
+
+const multiplicar = (a: number,b: number):number => {return a * b};
+
+console.log(multiplicar(9,5)); 
+
+// Função de saudação //
+
+// function ola (nome: String): string {
+//     return "Ola " + nome;
+// }
+
+const ola = (nome:string):string => {return "Olá " + nome}
+
+console.log(ola('Lucas')); 
